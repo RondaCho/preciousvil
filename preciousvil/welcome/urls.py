a3e31c96fe82327d455bef7bf88e43e4840e8096
RondaCho <duswls9292@naver.com>
2017-10-07 13:28:46 +0900

    dashboard만드는중

--- preciousvil/welcome/urls.py
+++ preciousvil/welcome/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='welcome'),
+    url(r'^dashboard/$', views.dash_board, name='dash_board'),
 ]
--- preciousvil/welcome/views.py
+++ preciousvil/welcome/views.py
@@ -1,5 +1,13 @@
 #welcome/views.py
 from django.shortcuts import render
+from .models import Progress
 
 def main_page(request):
     return render(request, 'welcome/main_page.html')
+
+def dash_board(request):
+    qs = Progress.objects.all()
+
+    return render(request, 'welcome/dash_board.html', {
+        'progress' : qs
+    })
